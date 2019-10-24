ae849a6401e4400d5c44105a74d249b4422e341e
Elia1996 <s223202@studenti.polito.it>
2019-10-24 18:44:32 +0200

    Testbench set as teacher wants

--- Lab1/4_Design_and_Sim/tb/IIR_tb.v
+++ Lab1/4_Design_and_Sim/tb/IIR_tb.v
@@ -6,9 +6,6 @@ module tb_fir ();
    wire CLK_i;
    wire RST_n_i;
    wire [11:0] DIN_i;
-   wire [11:0] v_filter_i;
-   wire [11:0] v1_filter_i;
-   wire [11:0] v2_filter_i;
    wire VIN_filter;
    wire VIN_i;
    wire [11:0] DOUT_i;
@@ -31,11 +28,7 @@ module tb_fir ();
 	     .DIN(DIN_i),
          .VIN(VIN_i),
          .DOUT(DOUT_i),
-         .VOUT(VOUT_i),
-		 .v(v_filter),
-	     .v1(v1_filter_i),
-	     .v2(v2_filter_i));   
-	     );
+         .VOUT(VOUT_i));
 
    IIR_tb_out TB_OUT(
 	    .CLK(CLK_i),
@@ -45,10 +38,7 @@ module tb_fir ();
 		//Vin e Din del filtro sono prese direttamente dal
 		// testbench precendente.
 		.VIN_filter(VIN_i),
-		.DIN_filter(VIN_i),
-		.v_filter(v_filter),
-	    .v1_filter(v1_filter_i),
-	    .v2_filter(v2_filter_i));   
+		.DIN_filter(VIN_i));
 endmodule
 
 
--- Lab1/4_Design_and_Sim/tb/IIR_tb_out.vhd
+++ Lab1/4_Design_and_Sim/tb/IIR_tb_out.vhd
@@ -8,77 +8,69 @@ use work.type_for_IIR_pkg.all;
 library std;
 use std.textio.all;
 
-entity data_sink is
-  port (
-    CLK   : in std_logic;
-    RST_n : in std_logic;
-    VIN   : in std_logic;
-    DIN   : in std_logic_vector(Nb-1 downto 0);
-  	VIN_filter: in std_logic;
-  	DIN_filter, v_filter, v1_filter, v2_filter: in std_logic_vector(Nb-1 downto 0));
-end data_sink;
+entity IIR_tb_out is
+	port (
+  		  CLK   : in std_logic;
+  		  RST_n : in std_logic;
+  		  VIN   : in std_logic;
+  		  DIN   : in std_logic_vector(Nb-1 downto 0);
+  		  VIN_filter: in std_logic;
+  	      DIN_filter: in std_logic_vector(Nb-1 downto 0));
+	end IIR_tb_out;
 
-architecture beh of data_sink is
+	architecture beh of IIR_tb_out is
 
 begin  -- beh
 
-  process (CLK, RST_n)
-        file res_fp : text open WRITE_MODE is "../sim_out/results_v.txt";
-	file res_csv_fp : text open WRITE_MODE is "../sim_out/results_v.csv";
-	file res_correct : text open READ_MODE is "../../3_C/results_c.txt";
-    variable line_out, line_csv_out, line_dout_correct : line;
-    variable int_from_c : integer;
-    variable flag : boolean := false;
+ 	process (CLK, RST_n)
+   		 file res_fp : text open WRITE_MODE is "../sim_out/results_v.txt";
+   		 file res_csv_fp : text open WRITE_MODE is "../sim_out/results_v.csv";
+   		 file res_correct : text open READ_MODE is "../../3_C/results_c.txt";
+   		 variable line_out, line_csv_out, line_dout_correct : line;
+   		 variable int_from_c : integer;
+   		 variable flag : boolean := false;
 
     begin
-     
-    if (flag=false) then
-	-- scrivo la prima linea del file csv
-	write(line_csv_out, string'("#VIN,DIN,v,v1,v2,VOUT,DOUT,DOUT_correct,Error"));
-	writeline(res_csv_fp, line_csv_out);
-    end if;
+   	 
+   		 if (flag=false) then
+   		 	-- scrivo la prima linea del file csv
+   		 	write(line_csv_out, string'("#VIN,DIN,VOUT,DOUT,DOUT_correct,Error"));
+   		 	writeline(res_csv_fp, line_csv_out);
+   		 end if;
 
-    flag := true;
+   		 flag := true;
 
-	if RST_n = '0' then                 -- asynchronous reset (active low)
-      		null;
-    	elsif CLK'event and CLK = '1' then  -- rising clock edge
-      	
-	if (VIN = '1') then
-		
-            write(line_out, conv_integer(signed(DIN)));
-		
-		write(line_csv_out, VIN_filter);
-	    write(line_csv_out, string'(","));
-	    	write(line_csv_out, conv_integer(signed(DIN_filter)));
-	    write(line_csv_out, string'(","));
-		write(line_csv_out, conv_integer(signed(v_filter)));
-	    write(line_csv_out, string'(","));
-		write(line_csv_out, conv_integer(signed(v1_filter)));
-	    write(line_csv_out, string'(","));
-		write(line_csv_out, conv_integer(signed(v2_filter)));
-	    write(line_csv_out, string'(","));
-		write(line_csv_out, VIN);
-	    write(line_csv_out, string'(","));
-		write(line_csv_out, conv_integer(signed(DIN)));
-	    	write(line_csv_out, string'(","));
-		write(line_csv_out, int_from_c);
-	    	write(line_csv_out, string'(","));
-		
-		readline(res_correct, line_dout_correct);
-		read(line_dout_correct, int_from_c);
-		
-		
-		if (int_from_c = conv_integer(signed(DIN))) then
-			write(line_csv_out,string'("OK,"));
-		else 
-			write(line_csv_out,string'("Error,"));
-		end if;
-        writeline(res_fp, line_out);
-	writeline(res_csv_fp, line_csv_out);
-	
-      end if;
-    end if;
+   		 if RST_n = '0' then                 -- asynchronous reset (active low)
+   		   	null;
+   		 elsif CLK'event and CLK = '1' then  -- rising clock edge	
+   		 	if (VIN = '1') then
+   		 		write(line_out, conv_integer(signed(DIN)));
+   		 	
+   		 		write(line_csv_out, VIN_filter);
+   		 	    write(line_csv_out, string'(","));
+   		 	    write(line_csv_out, conv_integer(signed(DIN_filter)));
+   		 	    write(line_csv_out, string'(","));
+   		 		write(line_csv_out, VIN);
+   		 	    write(line_csv_out, string'(","));
+   		 		write(line_csv_out, conv_integer(signed(DIN)));
+   		 	    write(line_csv_out, string'(","));
+   		 		write(line_csv_out, int_from_c);
+   		 	   	write(line_csv_out, string'(","));
+   		 		
+   		 		readline(res_correct, line_dout_correct);
+   		 		read(line_dout_correct, int_from_c);
+   		 	
+				assert (int_from_c = conv_integer(signed(DIN)) 
+					report "Output is wrong" severity warning;
+   		 		if (int_from_c = conv_integer(signed(DIN))) then
+   		 			write(line_csv_out,string'("OK,"));
+   		 		else 
+   		 			write(line_csv_out,string'("Error,"));
+   		 		end if;
+   		 	    writeline(res_fp, line_out);
+   		 		writeline(res_csv_fp, line_csv_out);
+   		 	end if;
+   		 end if;
 
   end process;
 
