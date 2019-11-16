41f382e74177dc3b37146e4fc80a7d377c4f02bc
Elia1996 <ribaldoneelia@gmail.com>
2019-11-16 21:38:42 +0100

    creato script per generare automaticamente il report power finale

--- Lab1/4_Design_and_Sim/sim/sim_saif.sh
+++ Lab1/4_Design_and_Sim/sim/sim_saif.sh
@@ -2,9 +2,9 @@
 echo "--------------- sim_saif  starts ------------------------------------"
 echo "-Elimination and re-creation of work dir"
 rm -r work
-vlib work
 echo "-Setting of 6.2g version of modelsim"
 source /software/scripts/init_msim6.2g > /dev/null
+vlib work
 echo "-Execution of modelsim, output log in saif/log directory"
 vsim -c -do compile_saif.do > ../saif/log/vsim_log.txt
 cat ../saif/log/vsim_log.txt | grep -i error
@@ -12,3 +12,6 @@ cat ../saif/log/vsim_log.txt | grep -i warning
 echo "-Elimination of wlf file"
 rm wlf*
 echo -e "--------------- end\n\n"
+# converto in saif
+source /software/scripts/init_synopsys_64.18
+vcd2saif -input ../vcd/IIR_filter.vcd -output ../saif/IIR_filter.saif
