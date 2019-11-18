7a81c9dd72f529c045c4979316f0cd30693f2eb2
Elia1996 <ribaldoneelia@gmail.com>
2019-11-18 12:25:24 +0100

    Modificato il file synt/saif.sh per eseguire il saif in modo automatico

--- Lab1/4_Design_and_Sim/synt/total_synt.sh
+++ Lab1/4_Design_and_Sim/synt/total_synt.sh
@@ -2,16 +2,11 @@
 
 dir=$(pwd)
 # tclk minimo 
-tmin=2.85
-# tclk minimo * 4 
-tmax=11.40
+tclk=$1
+ext=$2
 
 # sintesi dell'architettura col clock massimo per torvare l'area 
-/home/isa22/script/lib/sepf.sh -f $dir/variables_gen.scr -d $dir/variables.scr -p PERIOD,EXTENSION -n $tmin,_period  
+/home/isa22/script/lib/sepf.sh -f $dir/variables_gen.scr -d $dir/variables.scr -p PERIOD,EXTENSION -n $tclk,$ext
 echo "Creation of variables.scr with tclk min"
 ./synt_save.sh
 
-# sintesi dell'architettura col clock massimo*4 per trovarei area la potenza
-/home/isa22/script/lib/sepf.sh -f $dir/variables_gen.scr -d $dir/variables.scr -p PERIOD,EXTENSION -n $tmax,_period_x4  
-echo "Creation of variables.scr with tclk min*4"
-./synt_save.sh
