ba16ea0219da9055eaa9aca349f056b35ba85b3e
Marcello996 <lp19.17@localhost.localdomain>
2019-11-19 18:19:20 +0100

    La sintesi col registro nuovo funziona

--- Lab2/4_Design_and_Sim/synt/total_synt.sh
+++ Lab2/4_Design_and_Sim/synt/total_synt.sh
@@ -5,9 +5,15 @@ source $BASHLIB
 dir=$(pwd)
 t=$1
 ext=$2
+arch=$3
 
 # sintesi dell'architettura col clock massimo per torvare l'area 
-sepf -f $dir/variables_gen.scr -d $dir/variables.scr -p PERIOD,EXTENSION -n $t,$ext 
+sepf -f $dir/variables_gen.scr -d $dir/variables.scr -p PERIOD,EXTENSION,ARCH -n $t,$ext,$arch 
+if [[ $arch = "noarch" ]]; then
+	cat $dir/synt_gen.scr | sed 's/COMMENT/#/g'  > $dir/synt.scr
+else
+	cat $dir/synt_gen.scr | sed 's/COMMENT//g'  > $dir/synt.scr
+fi
 echo "Creation of variables.scr with tclk min"
 ./synt_save.sh
 
